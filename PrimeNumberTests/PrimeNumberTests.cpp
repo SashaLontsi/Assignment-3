76d6e7e76ac4327c1667e2cb186e11b09176766b
SashaLontsi <149993508+SashaLontsi@users.noreply.github.com>
2025-03-19 19:30:05 -0400

    Add project files.

---
+++ PrimeNumberTests/PrimeNumberTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PrimeNumberTests
+{
+	TEST_CLASS(PrimeNumberTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
